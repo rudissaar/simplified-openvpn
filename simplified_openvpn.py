b2def58515f0d1b3cc386176399fe8e0a8a27253
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2017-07-20 13:31:19 +0000

    Development, added set_clients_dir method.

--- simplified_openvpn.py
+++ simplified_openvpn.py
@@ -7,7 +7,25 @@ import pystache
 import zipfile
 
 class SimplifiedOpenVPN:
-    settings = []    
+    settings = dict()
 
+    def __init__(self):
+        self.settings['clients_dir'] = '/root/openvpn-clients/'
+        self.settings['server_dir'] = '/etc/openvpn/'
+        self.settings['easy_rsa_dir'] = '/etc/openvpn/easy-rsa/'
+
+    def sanitize_path(self, path):
+        if not path.endswith('/'):
+            path = path + '/'
+        return path
+
+    def set_clients_dir(self, clients_dir):
+        clients_dir = self.sanitize_path(clients_dir)
+        if not os.path.isdir(clients_dir):
+            return False
+        else:
+            self.settings['clients_dir'] = clients_dir
+            return True
+        
     def create_client(self):
-        pass
+        print(self.settings)
--- sovpn_client_create.py
+++ sovpn_client_create.py
@@ -2,4 +2,5 @@
 from simplified_openvpn import SimplifiedOpenVPN
 
 sovpn = SimplifiedOpenVPN()
+sovpn.set_clients_dir('/home')
 sovpn.create_client()
